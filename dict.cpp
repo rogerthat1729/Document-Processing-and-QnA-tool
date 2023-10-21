06b4bf54b2516f997c8ab4946cf7889929a8be49
T_Bone <harshal.sindal.12@gmail.com>
2023-10-21 19:19:13 +0530

    c2

--- dict.cpp
+++ dict.cpp
@@ -4,6 +4,7 @@ const int md = 200003;
 
 
 struct DictNode{
+public:
     int count=0;
     string word;
 };
@@ -17,8 +18,43 @@ Dict::Dict(){
 Dict::~Dict(){
 }
 
+vector<string> sent_to_words(string sentence){
+    int i=0;
+    vector<string> words;
+    while(i<sentence.size()){
+        string curword="";
+        while(((int(sentence[i])<=122 && int(sentence[i])>=97) || (int(sentence[i])<=90 && int(sentence[i]>=65))) && i<sentence.size()){
+            if(int(sentence[i])<=122 && int(sentence[i])>=97){
+                curword+=sentence[i];
+            }
+            else{
+                curword+=char(int(sentence[i])+32);
+            }
+            i++;
+        }
+        words.push_back(curword);
+        i++;
+    }
+    return words;
+}
+
 void Dict::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence){
-    // Implement your function here  
+    vector<string> sent_words=sent_to_words(sentence);
+    for(string curword: sent_words){
+        if(curword!=""){
+            int hashval=hash(curword);
+            int probe=0;
+            while(probe<words[hashval].size() && words[hashval][probe].word!=curword){
+                probe++;
+            }
+            if(probe==words[hashval].size()){
+                DictNode newword; 
+                newword.word=curword;
+                words[hashval].push_back(newword);
+            }
+            words[hashval][probe].count++;
+        }
+    }
     return;
 }
 
