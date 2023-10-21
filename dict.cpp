e4bc7f018da930e49c25cac2959af2cb42108c51
Vedant <cs1221603@iitd.ac.in>
2023-10-21 19:00:07 +0530

    included dump_dictionary and get_word_count

--- dict.cpp
+++ dict.cpp
@@ -2,41 +2,64 @@
 #include "dict.h"
 const int md = 200003;
 
+int hash(string s)
+{
+    int h = 5381;
+    int n = s.length();
+    for (int i = 0; i < n; ++i)
+    {
+        h = (33 * h + s[i]) % md;
+    }
+    return h;
+}
 
-struct DictNode{
-    int count=0;
+struct DictNode
+{
+    int count = 0;
     string word;
 };
 
-Dict::Dict(){
+Dict::Dict()
+{
     vector<vector<DictNode>> make(200003);
-    words=make;
-    size=0;
+    words = make;
+    size = 0;
 }
 
-Dict::~Dict(){
+Dict::~Dict()
+{
 }
 
-void Dict::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence){
-    // Implement your function here  
+void Dict::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence)
+{
+    // Implement your function here
     return;
 }
 
-int Dict::get_word_count(string word){
+int Dict::get_word_count(string word)
+{
+    int h = hash(word);
+    int n = words[h].size();
+    for (int i = 0; i < n; ++i)
+    {
+        if (word == words[h][i].word)
+            return words[h][i].count;
+    }
+    return 0;
     return -1;
 }
 
-void Dict::dump_dictionary(string filename){
-    // Implement your function here  
+void Dict::dump_dictionary(string filename)
+{
+    std::ofstream f;
+    f.open(filename);
+    for(auto &x : words){
+        int n = x.size();
+        for(int i = 0 ; i < n ; ++i)
+        {
+            f << x[i].word << ", " << x[i].count << endl;
+        }
+    }
+    f.close();
     return;
-}
-
-int hash(string s){
-    int h = 5381;
-    int n = s.length();
-    for(int i = 0 ; i < n ; ++i)
-    {
-        h = (33*h + s[i])%md;
-    }   
-    return h;
 }
