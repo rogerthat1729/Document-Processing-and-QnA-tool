936d428fe69c907caa41769934b65a352e152d2a
cs1597 <121373727+cs1597@users.noreply.github.com>
2023-10-24 23:17:14 +0530

    c1

--- search.cpp
+++ search.cpp
@@ -14,6 +14,26 @@ void SearchEngine::insert_sentence(int book_code, int page, int paragraph, int s
     return;
 }
 
+vector<int> KMPTable(string s){
+    vector<int> t(s.size());
+    int pos=1; int cnd=0;
+    while(pos<s.size()){
+        if(s[pos]==s[cnd]){
+            t[pos]=t[cnd];
+        }
+        else{
+            t[pos]=cnd;
+            while(cnd>=0 && s[pos]!=s[cnd]){
+                cnd=t[cnd];
+            }
+        }
+        pos++;
+        cnd++
+    }
+    t[pos]=cnd;
+    return t;
+}
+
 Node* SearchEngine::search(string pattern, int& n_matches){
     // Implement your function here  
     return nullptr;
