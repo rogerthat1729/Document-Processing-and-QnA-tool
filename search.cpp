d8819acbaf917771bc0d8796f30b69b136cf8ece
Vedant <cs1221603@iitd.ac.in>
2023-10-24 23:35:04 +0530

    ok

--- search.cpp
+++ search.cpp
@@ -11,9 +11,42 @@ SearchEngine::~SearchEngine(){
 
 void SearchEngine::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence)
 {
+    for(int i = 0; i < sentence.length(); i++)
+    {
+        if (int(sentence[i]) <= 90 && int(sentence[i]) >= 65)
+        {
+            sentence[i] += 32;
+        }
+    }
     sentences.push_back(sentence);
     data.push_back({book_code, page, paragraph, sentence_no});
     return;
+}
+
+vector<int> KMPTable(string s)
+{
+    vector<int> t(s.size());
+    int pos = 1;
+    int cnd = 0;
+    while (pos < s.size())
+    {
+        if (s[pos] == s[cnd])
+        {
+            t[pos] = t[cnd];
+        }
+        else
+        {
+            t[pos] = cnd;
+            while (cnd >= 0 && s[pos] != s[cnd])
+            {
+                cnd = t[cnd];
+            }
+        }
+        pos++;
+        cnd++
+    }
+    t[pos] = cnd;
+    return t;
 }
 
 Node *SearchEngine::search(string pattern, int &n_matches)
